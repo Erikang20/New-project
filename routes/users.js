a1657495859724167baab7e0848ac6fe4322a594
Erikang20 <erikang20@gmail.com>
2016-06-27 21:31:37 -0500

    add CRUD

--- routes/users.js
+++ routes/users.js
@@ -15,62 +15,6 @@ router.get( '/', function( req, res ) {
 } );
 
 
-///post to users table
-router.post( '/', function( req, res ) {
-	var user = req.body;
-	// eval( locus )
-	knex( 'users' ).insert( {
-		name: user.name,
-		userName: user.userName
-	} ).then( function( result, err ) {
-		res.redirect( '/users' );
-	} );
-} );
-
-
-//add new users
-router.get( '/new', function( req, res ) {
-	res.render( 'new' );
-} );
-
-
-
-///get user id
-router.get( '/:id', function( req, res ) {
-	var userId = req.params.id;
-	knex( 'users' ).where( 'id', userId ).first().then( function( result, err ) {
-		var user = result;
-		// eval( locus )
-		res.render( 'show', {
-			user: user
-		} );
-	} );
-} );
-
-
-
-//edit
-router.get( '/:id/edit', function( req, res ) {
-	var userId = req.params.id;
-	knex( 'users' ).where( 'id', userId ).select().then( function( result, err ) {
-		// var user = result;
-		res.render( 'edit', {
-			user: result[ 0 ]
-		} );
-	} );
-} );
-
-router.post( '/:id', function( req, res ) {
-	var userId = req.params.id;
-	var user = req.body;
-	knex( 'users' ).where( 'id', userId ).first().update( {
-		name: user.name,
-		userName: user.userName
-	} ).then( function( result ) {
-		res.redirect( '/users' );
-	} );
-} );
-
 
 //delete
 router.get( '/:id', function( req, res ) {
