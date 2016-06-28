8d8d26880aad8e8d8510046eacaa6c559b15aa56
Erikang20 <erikang20@gmail.com>
2016-06-28 16:12:56 -0500

    add stuff

--- public/main.js
+++ public/main.js
@@ -1,22 +1,21 @@
 // parallax section
 // ------------------------------------------
-// $( document ).ready( function() {
-// $( window ).trigger( 'scroll' );
-// $( '.parallax-window' ).load( function( parallax ) {
-// 	naturalWidth = 600,
-// 		naturalHeight = 200
-// 	console.log( "it works" );
-//
-// } );
+$( document ).ready( function() {
+	$( window ).trigger( 'scroll' );
+	$( '.parallax-window' ).load( function( parallax ) {
+		naturalWidth = 600,
+			naturalHeight = 200
+		console.log( "it works" );
 
-$( document ).ready( function( parallax ) {
-	$( '.parallax-window' ).parallax( {
-		imageSrc: 'https://images.unsplash.com/photo-1414277578841-112f6e9398ec?crop=entropy&fit=crop&fm=jpg&h=800&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1375'
 	} );
 
+	$( document ).ready( function( parallax ) {
+		$( '.parallax-window' ).parallax( {
+			imageSrc: 'https://images.unsplash.com/photo-1414277578841-112f6e9398ec?crop=entropy&fit=crop&fm=jpg&h=800&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1375'
+		} );
+	} );
 } );
 
-// } );
 
 
 
