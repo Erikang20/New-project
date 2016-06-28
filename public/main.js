f023d14c33cfd2f31b8306dd6e872538f51fb367
Aaron Levinson <aaron.levinson1000@gmail.com>
2016-06-28 09:53:40 -0500

    Fixing errors

--- public/main.js
+++ public/main.js
@@ -2,28 +2,28 @@
 // ------------------------------------------
 $( document ).ready( function () {
 
-			$( window ).trigger( 'scroll' );
+	$( window ).trigger( 'scroll' );
 
-			$( '.parallax-window' ).load( function ( parallax ) {
-				// naturalWidth: 600,
-				// naturalHeight: 200
-			} );
+	$( '.parallax-window' ).load( function ( parallax ) {
+		// naturalWidth: 600,
+		// naturalHeight: 200
+	} );
+} );
+console.log( "it works" );
 
-			console.log( "it works" );
+$( window ).load( function () {
+	$( "#preloader" ).fadeOut( "slow" );
 
-			$( window ).load( function () {
-				$( "#preloader" ).fadeOut( "slow" );
+	// animation when the page is being scroll
+	$( window ).scroll( function () {
+		if ( $( window ).scrollTop() > 400 ) {
+			$( ".navbar-brand a" ).css( "color", "#fff" );
+			$( "#navigation" ).removeClass( "animated-header" );
+		}
+		else {
+			$( ".navbar-brand a" ).css( "color", "inherit" );
+			$( "#navigation" ).addClass( "animated-header" );
+		}
+	} );
 
-				// animation when the page is being scroll
-				$( window ).scroll( function () {
-					if ( $( window ).scrollTop() > 400 ) {
-						$( ".navbar-brand a" ).css( "color", "#fff" );
-						$( "#navigation" ).removeClass( "animated-header" );
-					}
-					else {
-						$( ".navbar-brand a" ).css( "color", "inherit" );
-						$( "#navigation" ).addClass( "animated-header" );
-					}
-				} );
-
-			} );
+} );
