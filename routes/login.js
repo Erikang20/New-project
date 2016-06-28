9a612eb419f60445339b90a1cae75593e5ffb7e1
Erikang20 <erikang20@gmail.com>
2016-06-28 13:51:40 -0500

    ready for deployment

--- routes/login.js
+++ routes/login.js
@@ -5,8 +5,8 @@ var methodOverride = require( 'method-override' );
 var bodyParser = require( 'body-parser' );
 
 /* GET home page. */
-router.get( '/', function ( req, res, next ) {
-	knex( 'users' ).select().then( function ( result, err ) {
+router.get( '/', function( req, res, next ) {
+	knex( 'users' ).select().then( function( result, err ) {
 		res.render( 'login', {
 			user: result
 
