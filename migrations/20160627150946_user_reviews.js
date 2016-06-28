0d53c632d3a8f8760fbd1a0a8f691084945687fd
Aaron Levinson <aaron.levinson1000@gmail.com>
2016-06-28 10:30:27 -0500

    Updated Migrations

--- migrations/20160627150946_user_reviews.js
+++ migrations/20160627150946_user_reviews.js
@@ -1,19 +1,19 @@
 exports.up = function ( knex, Promise ) {
-		return knex.schema.createTable( 'user_reviews', function ( table ) {
-			table.increments();
-			table.text( 'review' );
-			table.integer( 'written_for' ).unsigned().index().references( 'users.id' )
-				.onDelete(
-					'CASCADE' );
-			table.integer( 'written_by' ).unsigned().index().references( 'users.id' ).onDelete(
+	return knex.schema.createTable( 'user_reviews', function ( table ) {
+		table.increments();
+		table.text( 'review' );
+		table.integer( 'written_for' ).unsigned().index().references( 'users.id' )
+			.onDelete(
 				'CASCADE' );
+		table.integer( 'written_by' ).unsigned().index().references( 'users.id' ).onDelete(
+			'CASCADE' );
 
-		} );
-	},
-
-
-	exports.down = function ( knex, Promise ) {
-		return knex.schema.dropTable( 'user_reviews' );
+	} );
+};
 
 
-	};
+exports.down = function ( knex, Promise ) {
+	return knex.schema.dropTable( 'user_reviews' );
+
+
+};
--- migrations/20160627151004_dog_reviews.js
+++ migrations/20160627151004_dog_reviews.js
@@ -1,5 +1,5 @@
 exports.up = function ( knex, Promise ) {
-	return knex.schema.createTable( 'dogs_reviews', function ( table ) {
+	return knex.schema.createTable( 'dog_reviews', function ( table ) {
 		table.increments();
 		table.text( 'review' );
 		table.integer( 'written_for' ).unsigned().index().references( 'dogs.id' ).onDelete(
@@ -13,7 +13,7 @@ exports.up = function ( knex, Promise ) {
 };
 
 exports.down = function ( knex, Promise ) {
-	return knex.schema.dropTable( 'dogs_reviews' );
+	return knex.schema.dropTable( 'dog_reviews' );
 
 
 };
