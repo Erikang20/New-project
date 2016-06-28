8edbeaddc1b8f1cbb7ebf18ff1ec076ab07f4a71
Erikang20 <erikang20@gmail.com>
2016-06-28 16:36:09 -0500

    add changes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var bodyParser = require( 'body-parser' );
 var methodOverride = require( 'method-override' );
 var passport = require( 'passport' );
 var FacebookStrategy = require( 'passport-facebook' ).Strategy;
+var knex = require( 'knex' );
 var app = express();
 
 
